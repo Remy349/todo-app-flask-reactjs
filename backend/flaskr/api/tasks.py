7165f7ca42a86af25bab8422fc132499b3518134
Remy349 <santoraga15@gmail.com>
2022-12-04 21:12:02 -0600

    Implement API pagination and complete the routes with GET methods

--- backend/flaskr/api/tasks.py
+++ backend/flaskr/api/tasks.py
@@ -1,17 +1,26 @@
 from flaskr.api import bp
 from flaskr import db
+from flask import jsonify, request
 
 from flaskr.models import Task
 
 
 @bp.route("/tasks/<int:id_task>", methods=["GET"])
 def get_task(id_task):
-    return f"Task: {id_task}"
+    task = db.get_or_404(Task, id_task)
+
+    return jsonify(task.to_dict())
 
 
 @bp.route("/tasks", methods=["GET"])
 def get_tasks():
-    return "All tasks"
+    page = request.args.get("page", 1, type=int)
+    per_page = min(request.args.get("per_page", 6, type=int), 100)
+
+    data = Task.to_collection_dict(db.select(Task).order_by(
+        Task.id_task), page, per_page, "api.get_tasks")
+
+    return data
 
 
 @bp.route("/tasks", methods=["POST"])
--- backend/flaskr/models.py
+++ backend/flaskr/models.py
@@ -1,3 +1,4 @@
+from flask import url_for
 import sqlalchemy as sa
 from datetime import datetime
 from flaskr import db
@@ -8,6 +9,45 @@ class Task(db.Model):
     title = sa.Column(sa.String(50), nullable=False)
     description = sa.Column(sa.String(150), nullable=False)
     timestamp = sa.Column(sa.DateTime, index=True, default=datetime.utcnow)
+
+    def to_dict(self):
+        data = {
+            "id_task": self.id_task,
+            "title": self.title,
+            "description": self.description,
+            "timestamp": self.timestamp
+        }
+
+        return data
+
+    def from_dict(self, data):
+        for field in ["title", "description"]:
+            if field in data:
+                setattr(self, field, data[field])
+
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = db.paginate(query)
+
+        data = {
+            "items": [item.to_dict() for item in resources.items],
+            "meta": {
+                "page": page,
+                "per_page": per_page,
+                "total_pages": resources.pages,
+                "total_items": resources.total
+            },
+            "links": {
+                "self": url_for(endpoint, page=page, per_page=per_page,
+                                **kwargs),
+                "next": url_for(endpoint, page=page + 1, per_page=per_page,
+                                **kwargs) if resources.has_next else None,
+                "prev": url_for(endpoint, page=page - 1, per_page=per_page,
+                                **kwargs) if resources.has_prev else None
+            }
+        }
+
+        return data
 
     def __repr__(self):
         return f"""
