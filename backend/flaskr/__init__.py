b970c808d853f558c42a59adc3feeb517adb55c3
Remy349 <santoraga15@gmail.com>
2022-12-07 12:39:51 -0600

    Established connection between server and client, function to obtain all data done

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -19,6 +20,8 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     migrate.init_app(app, db, compare_type=True)
+
+    CORS(app)
 
     from flaskr.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix="/api")
--- frontend/src/components/Article.jsx
+++ frontend/src/components/Article.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   FormTask,
   InputFormTask,
@@ -6,11 +6,48 @@ import {
   ButtonFormTask,
 } from './styled/FormStyled'
 
+const API = import.meta.env.VITE_API_URL
+
 export const Article = () => {
+  const [tasks, setTasks] = useState([])
+  const [inputs, setInputs] = useState({
+    title: '',
+    description: '',
+  })
+
+  const handleInputChange = (e) => {
+    const value = e.target.value
+    const name = e.target.name
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const getTasks = async () => {
+    const res = await fetch(`${API}/api/tasks`, {
+      method: 'GET',
+    })
+
+    const data = await res.json()
+
+    // console.log(data.items)
+    setTasks(data.items)
+  }
+
+  useEffect(() => {
+    getTasks()
+  }, [])
+
   return (
     <article className='article'>
       <section className='article__form'>
-        <FormTask className='form'>
+        <FormTask className='form' onSubmit={handleSubmit}>
           <div className='form__container'>
             <InputFormTask
               type='text'
@@ -18,6 +55,8 @@ export const Article = () => {
               placeholder='Title'
               className='form__input'
               autoComplete='off'
+              value={inputs.title}
+              onChange={handleInputChange}
             />
           </div>
           <div className='form__container'>
@@ -26,6 +65,8 @@ export const Article = () => {
               className='form__textarea'
               rows='4'
               placeholder='Description'
+              value={inputs.description}
+              onChange={handleInputChange}
             />
           </div>
           <div className='form__btn'>
@@ -35,7 +76,9 @@ export const Article = () => {
           </div>
         </FormTask>
       </section>
-      <section className='article__content'></section>
+      <section className='article__content'>
+        {tasks.length === 0 ? <h1>Empty</h1> : <div>{tasks[0].title}</div>}
+      </section>
     </article>
   )
 }
