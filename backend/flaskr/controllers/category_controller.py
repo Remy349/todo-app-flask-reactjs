f3bde64fab6a67997af1039a5008ca0bbeb596fd
Remy349 <santoraga15@gmail.com>
2023-12-14 23:46:35 -0600

    Refactor the category controller, service and repository. Make some testing.

--- backend/flaskr/controllers/category_controller.py
+++ backend/flaskr/controllers/category_controller.py
@@ -1,3 +1,4 @@
+from flask_jwt_extended import jwt_required
 from flask_smorest import Blueprint
 from flask.views import MethodView
 from flaskr.schemas.schema import CategorySchema
@@ -8,15 +9,19 @@ bp = Blueprint("categories", __name__, description="Operations on categories")
 category_service = CategoryService()
 
 
-@bp.route("/categories")
-class CategoriesController(MethodView):
+@bp.route("/users/<user_id>/categories")
+class CategoriesInUserController(MethodView):
     @bp.response(200, CategorySchema(many=True))
-    def get(self):
-        """Get a list of categories"""
-        return category_service.list_categories()
+    def get(self, user_id):
+        """Get a list of a user´s categories"""
+        return category_service.list_categories_in_user(user_id)
 
+    @jwt_required()
     @bp.arguments(CategorySchema)
     @bp.response(201, CategorySchema)
-    def post(self, category_data):
-        """Create a new category"""
-        return category_service.create_new_category(category_data)
+    def post(self, category_data, user_id):
+        """Create a new category in a user"""
+        return category_service.create_new_category_in_user(
+            category_data,
+            user_id,
+        )
--- backend/flaskr/services/category_service.py
+++ backend/flaskr/services/category_service.py
@@ -1,3 +1,4 @@
+from flask_smorest import abort
 from flaskr.models.category_model import CategoryModel
 from flaskr.repositories.category_repository import CategoryRepository
 from flaskr.repositories.user_repository import UserRepository
@@ -7,13 +8,21 @@ category_repository = CategoryRepository()
 
 
 class CategoryService:
-    def list_categories(self):
-        return category_repository.get_categories()
+    def list_categories_in_user(self, user_id):
+        return category_repository.get_categories_in_user(user_id)
 
-    def create_new_category(self, category_data):
-        user = user_repository.get_user_by_id(category_data["user_id"])
+    def create_new_category_in_user(self, category_data, user_id):
+        user = user_repository.get_user_by_id(user_id)
+
+        category_created = category_repository.get_category_in_user_by_category_name(
+            user_id,
+            category_data["category_name"],
+        )
+
+        if category_created:
+            abort(409)
 
         category = CategoryModel(**category_data)
         category.user = user
 
-        return category_repository.create_category(category)
+        return category_repository.create_category_in_user(category)
