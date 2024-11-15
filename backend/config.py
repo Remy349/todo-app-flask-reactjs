eddfd62e09c854bdedf9d160992ac1eb972596c7
Remy349 <santoraga15@gmail.com>
2024-11-14 21:51:03 -0600

    feat: Basic project strucuture and setup

---
+++ backend/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, ".env"))
+
+
+class Config(object):
+    pass
+
+
+class DevelopmentConfig(Config):
+    pass
+
+
+class TestConfig(Config):
+    pass
---
+++ backend/flaskr/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from config import DevelopmentConfig
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    if test_config is None:
+        app.config.from_object(DevelopmentConfig)
+    else:
+        app.config.from_object(test_config)
+
+    return app
