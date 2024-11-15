3a53b8e35eb5f3a761e7f58cc3087076d36525cd
Remy349 <santoraga15@gmail.com>
2024-11-15 11:17:53 -0600

    refactor: Change import classes from sqlalchemy

--- backend/flaskr/models/user_model.py
+++ backend/flaskr/models/user_model.py
@@ -1,16 +1,16 @@
-import sqlalchemy as sa
-import sqlalchemy.orm as so
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
 from flaskr.extensions import db
 
 
 class UserModel(db.Model):
     __tablename__ = "users"
 
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(
-        sa.String(20), nullable=False, unique=True, index=True
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(
+        String(20), nullable=False, unique=True, index=True
     )
-    email: so.Mapped[str] = so.mapped_column(
-        sa.String(120), nullable=False, unique=True, index=True
+    email: Mapped[str] = mapped_column(
+        String(120), nullable=False, unique=True, index=True
     )
-    password: so.Mapped[str] = so.mapped_column(sa.String(300), nullable=False)
+    password: Mapped[str] = mapped_column(String(300), nullable=False)
