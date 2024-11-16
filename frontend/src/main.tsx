7050bae6f73f8a0540a6d2ba5249a2a299cabfc2
Remy349 <santoraga15@gmail.com>
2024-11-16 00:53:08 -0600

    feat: Add shadcn ui and tailwind css support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
 import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Button } from "./components/ui/button";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>HOME PAGE</h1>
+    <Button>Click Me</Button>
   </StrictMode>,
 );
