019e910b618e109c83331b6a1a6d51289cefce96
Remy349 <santoraga15@gmail.com>
2024-11-17 22:19:21 -0600

    feat: Add try/except to handle errors and create user logic completed

--- backend/flaskr/controllers/user_controller.py
+++ backend/flaskr/controllers/user_controller.py
@@ -1,14 +1,18 @@
 from flask_smorest import abort
 from sqlalchemy import select
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.exc import NoResultFound, SQLAlchemyError
 from flaskr.db import db
 from flaskr.models.user_model import UserModel
+from flaskr.utils import generate_password
 
 
 class UserController:
     @staticmethod
     def get_all():
-        return db.session.execute(select(UserModel)).scalars().all()
+        try:
+            return db.session.execute(select(UserModel)).scalars().all()
+        except SQLAlchemyError:
+            abort(500, message="Internal server error while fetching users")
 
     @staticmethod
     def get_by_id(user_id):
@@ -18,10 +22,34 @@ class UserController:
             ).scalar_one()
         except NoResultFound:
             abort(404, message="User not found")
+        except SQLAlchemyError:
+            abort(500, message="Internal server error while fetching user")
 
     @staticmethod
     def create(data):
-        pass
+        try:
+            user_registered = db.session.execute(
+                select(UserModel).where(
+                    (UserModel.username == data["username"])
+                    | (UserModel.email == data["email"])
+                )
+            ).scalar_one_or_none()
+
+            if user_registered:
+                if user_registered.username == data["username"]:
+                    abort(409, message="Username already registered")
+                if user_registered.email == data["email"]:
+                    abort(409, message="Email already registered")
+
+            new_user = UserModel(**data)
+
+            new_user.password = generate_password(data["password"])
+
+            db.session.add(new_user)
+            db.session.commit()
+        except SQLAlchemyError:
+            db.session.rollback()
+            abort(500, message="Internal server error while creating user")
 
     @staticmethod
     def delete(user_id):
@@ -32,5 +60,8 @@ class UserController:
 
             db.session.delete(user)
             db.session.commit()
-        except:
+        except NoResultFound:
             abort(404, message="User not found")
+        except SQLAlchemyError:
+            db.session.rollback()
+            abort(500, message="Internal server error while deleting user")
