76dff4a014a746c5e01fa181c52f45d82ae47276
Remy349 <santoraga15@gmail.com>
2024-11-19 15:20:41 -0600

    feat: Add new path for dashboard

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import { LandingRoot } from "./landing/root";
 import { HomePage } from "./landing/home/page";
+import { DashboardRoot } from "./dashboard/root";
+import { DashboardHomePage } from "./dashboard/page";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingRoot />,
     children: [{ index: true, element: <HomePage /> }],
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardRoot />,
+    children: [{ index: true, element: <DashboardHomePage /> }],
   },
 ]);
