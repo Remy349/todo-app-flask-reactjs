f9fef0add24d8ec72a717c78d039bb35ced6e293
Remy349 <santoraga15@gmail.com>
2024-11-19 17:14:01 -0600

    remove: Create tag dialog from home

--- frontend/src/routes/dashboard/page.tsx
+++ frontend/src/routes/dashboard/page.tsx
@@ -1,17 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { PlusCircle } from "lucide-react";
-import { CreateTagDialog } from "./_components/tags/create-dialog";
-
 export const DashboardHomePage = () => {
   return (
     <div>
-      <div className="flex items-center gap-x-2">
-        <CreateTagDialog />
-        <Button size="sm" className="font-medium">
-          <PlusCircle />
-          Create new task
-        </Button>
-      </div>
+      <h1>DashboardHomePage</h1>
     </div>
   );
 };
