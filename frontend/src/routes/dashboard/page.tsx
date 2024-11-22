4b195a0e6e991306112725a9f322675dde86849b
Remy349 <santoraga15@gmail.com>
2024-11-21 19:33:08 -0600

    feat: Add tags section

--- frontend/src/routes/dashboard/page.tsx
+++ frontend/src/routes/dashboard/page.tsx
@@ -1,7 +1,12 @@
+import { TagsSection } from "./_components/tags/tags-section";
+
 export const DashboardHomePage = () => {
   return (
     <div>
-      <h1>DashboardHomePage</h1>
+      <h1 className="font-bold text-center text-2xl mb-2 md:mb-4">
+        Tags currently available
+      </h1>
+      <TagsSection />
     </div>
   );
 };
