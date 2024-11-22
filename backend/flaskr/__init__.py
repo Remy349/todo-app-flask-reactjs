8b6178760769c5762c6301a2d787b5db66692547
Remy349 <santoraga15@gmail.com>
2024-11-22 13:48:55 -0600

    feat: Add tasks api endpoints

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,6 +8,7 @@ from flaskr.db import db
 from flaskr.routes.auth_route import bp as auth_route
 from flaskr.routes.user_route import bp as user_route
 from flaskr.routes.tag_route import bp as tag_route
+from flaskr.routes.task_route import bp as task_route
 
 
 def create_app(test_config=None):
@@ -27,5 +28,6 @@ def create_app(test_config=None):
     api.register_blueprint(auth_route, url_prefix="/api/v1")
     api.register_blueprint(user_route, url_prefix="/api/v1")
     api.register_blueprint(tag_route, url_prefix="/api/v1")
+    api.register_blueprint(task_route, url_prefix="/api/v1")
 
     return app
