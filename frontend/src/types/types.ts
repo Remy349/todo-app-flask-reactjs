3969b8dd503d7e28a08f2e70dcf4e9a3c96e5a22
Remy349 <santoraga15@gmail.com>
2024-11-27 15:35:04 -0600

    feat: Add task type

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -2,3 +2,13 @@ export type Tag = {
   id: number;
   name: string;
 };
+
+export type Status = "PENDING" | "IN_PROGRESS" | "COMPLETED";
+
+export type Task = {
+  id: number;
+  title: string;
+  content: string;
+  status: Status;
+  createdAt: string;
+};
