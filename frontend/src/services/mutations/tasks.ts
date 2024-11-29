e0e98baea500a8d666c5f9d8aa30a08c3d9f2b38
Remy349 <santoraga15@gmail.com>
2024-11-29 15:27:58 -0600

    feat: Add delete task mutation

--- frontend/src/services/mutations/tasks.ts
+++ frontend/src/services/mutations/tasks.ts
@@ -1,11 +1,22 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createTaskAPI } from "../api/tasks";
+import { createTaskAPI, deleteTaskAPI } from "../api/tasks";
 
 export const useCreateTaskMutation = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: createTaskAPI,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+  });
+};
+
+export const useDeleteTaskMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteTaskAPI,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["tasks"] });
     },
