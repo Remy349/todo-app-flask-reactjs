c2dc0cb122c97b32b0bd8a9c15eff68938de2925
Remy349 <santoraga15@gmail.com>
2024-11-29 18:26:23 -0600

    feat: Add task api call for update

--- frontend/src/services/api/tasks.ts
+++ frontend/src/services/api/tasks.ts
@@ -1,4 +1,4 @@
-import { TCreateFormSchema } from "@/schemas/task-schema";
+import { TCreateFormSchema, TEditFormSchema } from "@/schemas/task-schema";
 import { useAuthStore } from "@/stores/auth-store";
 import { Task } from "@/types/types";
 import axios from "axios";
@@ -25,6 +25,20 @@ export const createTaskAPI = async (data: {
   const { formData, token } = data;
 
   await axios.post("http://localhost:5000/api/v1/tasks", formData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const updateTaskAPI = async (data: {
+  formData: TEditFormSchema;
+  taskId: number;
+  token: string | null;
+}) => {
+  const { formData, token, taskId } = data;
+
+  await axios.put(`http://localhost:5000/api/v1/tasks/${taskId}`, formData, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
