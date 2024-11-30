50ff374c3b6d49c7de80ca3cf4cd2734f79c2654
Remy349 <santoraga15@gmail.com>
2024-11-29 18:25:48 -0600

    feat: Add task mutation for update

--- frontend/src/services/mutations/tasks.ts
+++ frontend/src/services/mutations/tasks.ts
@@ -1,11 +1,22 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createTaskAPI, deleteTaskAPI } from "../api/tasks";
+import { createTaskAPI, deleteTaskAPI, updateTaskAPI } from "../api/tasks";
 
 export const useCreateTaskMutation = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: createTaskAPI,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+  });
+};
+
+export const useUpdateTaskMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateTaskAPI,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["tasks"] });
     },
